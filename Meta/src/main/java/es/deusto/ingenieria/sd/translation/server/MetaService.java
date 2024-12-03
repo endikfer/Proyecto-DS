08bf8e4024e3d344b72f23ca0c5ba30bbe4ea31d
Alexander Fernandez <alexander.f@opendeusto.es>
2024-12-03 08:59:17 +0100

    Cambios finales en el metodo log in y register

--- Meta/src/main/java/es/deusto/ingenieria/sd/translation/server/MetaService.java
+++ Meta/src/main/java/es/deusto/ingenieria/sd/translation/server/MetaService.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class MetaService extends Thread {
 	private DataInputStream in;
@@ -16,10 +17,12 @@ public class MetaService extends Thread {
 	private Socket tcpSocket;
 	private Map<String, String> emails = new HashMap<>();
 	
+	private static String DELIMITER = "#";
+
 	public MetaService(Socket socket) {
 		try {
 			this.tcpSocket = socket;
-		    this.in = new DataInputStream(socket.getInputStream());
+			this.in = new DataInputStream(socket.getInputStream());
 			this.out = new DataOutputStream(socket.getOutputStream());
 			this.start();
 		} catch (IOException e) {
@@ -31,34 +34,38 @@ public class MetaService extends Thread {
 		try {
 			String data = this.in.readUTF();
 			String respuesta;
-			System.out.println("   - MetaService - Datos recibidos de '" + tcpSocket.getInetAddress().getHostAddress() + ":" + tcpSocket.getPort() + "' -> '" + data + "'");					
-			
+			System.out.println("   - MetaService - Datos recibidos de '" + tcpSocket.getInetAddress().getHostAddress()
+					+ ":" + tcpSocket.getPort() + "' -> '" + data + "'");
+
 			if (data.contains("#")) {
-				//Login
-				String[] parts = data.split("#");
-                String email = parts[0];
-                String password = parts[1];
-				respuesta= LogIn(email, password); //llama al metodo necesario aqui
+				// Login
+				StringTokenizer tokenizer = new StringTokenizer(data, DELIMITER);
+				String email = tokenizer.nextToken();
+				String password = tokenizer.nextToken();
+				respuesta = LogIn(email, password); // llama al metodo necesario aqui
 				this.out.writeUTF(respuesta);
-			}else if(data.matches("[^@]+@[^@]+\\.[^@]+")) {
-				//registro
-				respuesta= Register(data); //llama al metodo necesario aqui
+
+			} else if (data.matches("[^@]+@[^@]+\\.[^@]+")) {
+				// registro
+				respuesta = Register(data); // llama al metodo necesario aqui
 				this.out.writeUTF(respuesta);
-			}else {
+			} else {
 				this.out.writeUTF("Error: Formato de datos no válido.");
-	            System.out.println("   - MetaService - Formato de datos no válido: " + data);
+				System.out.println("   - MetaService - Formato de datos no válido: " + data);
 			}
-					
-			this.out.writeUTF(data);					
-			System.out.println("   - MetaService - datos enviados a " + tcpSocket.getInetAddress().getHostAddress() + ":" + tcpSocket.getPort() + "' -> '" + data.toUpperCase() + "'");
-			
-			/*if (loginWithMeta(data)) {
-                this.out.writeUTF("Login exitoso. Tu token de sesión es: TOKEN");
-                System.out.println("   - MetaService - Login exitoso. Token enviado: TOKEN");
-            } else {
-                this.out.writeUTF("Error de autenticación con Meta.");
-                System.out.println("   - MetaService - Error de autenticación para el email: " + data);
-            }*/
+
+			this.out.writeUTF(data);
+			System.out.println("   - MetaService - datos enviados a " + tcpSocket.getInetAddress().getHostAddress()
+					+ ":" + tcpSocket.getPort() + "' -> '" + data.toUpperCase() + "'");
+
+			/*
+			 * if (loginWithMeta(data)) {
+			 * this.out.writeUTF("Login exitoso. Tu token de sesión es: TOKEN");
+			 * System.out.println("   - MetaService - Login exitoso. Token enviado: TOKEN");
+			 * } else { this.out.writeUTF("Error de autenticación con Meta."); System.out.
+			 * println("   - MetaService - Error de autenticación para el email: " + data);
+			 * }
+			 */
 		} catch (EOFException e) {
 			System.err.println("   # MetaService - TCPConnection EOF error" + e.getMessage());
 		} catch (IOException e) {
@@ -71,43 +78,48 @@ public class MetaService extends Thread {
 				System.err.println("   # MetaService - TCPConnection IO error:" + e.getMessage());
 			}
 		}
-		
+
 	}
-	
-	/*private boolean loginWithMeta(String email/*, String password) {
-        String accessToken = authenticateWithMetaAPI(email/*, password);
-        return accessToken != null && !accessToken.isEmpty();
-    }
-	
-	
-	private String authenticateWithMetaAPI(String email/*, String password) {
-        if (email.contains("@meta.com")/* && password.equals("12345")){
-            return "fake_facebook_access_token";
-        }
-        return null;
-    }*/
-	
+
+	/*
+	 * private boolean loginWithMeta(String email/*, String password) { String
+	 * accessToken = authenticateWithMetaAPI(email/*, password); return accessToken
+	 * != null && !accessToken.isEmpty(); }
+	 * 
+	 * 
+	 * private String authenticateWithMetaAPI(String email/*, String password) { if
+	 * (email.contains("@meta.com")/* && password.equals("12345")){ return
+	 * "fake_facebook_access_token"; } return null; }
+	 */
+
 	private String LogIn(String email, String contraseña) {
-		
-		if(emails.containsKey(email)) {
-			if(emails.get(email).equals(contraseña)) {
-				return "OK";
-			}else {
-				return "La contraseña es incorrecta";
+		String respuesta = null;
+
+		if (email != null && contraseña != null) {
+			
+			DataChecker DC = new DataChecker();
+			try {
+				respuesta = DC.LogIn(email, contraseña);
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
-		return "El email no esta registrado en Meta";
-		
-		
+		return (respuesta == null) ? "ERR" : "OK";
+
 	}
-	
+
 	private String Register(String email) {
-		if(!emails.containsKey(email)) {
-			emails.put(email, null);
-			return "OK";
+		String respuesta = null;
+
+		if (email != null) {
+			
+			DataChecker DC = new DataChecker();
+			try {
+				respuesta = DC.Register(email);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-		
-		return "El email ya esta registrado";
-	}
-	
+		return (respuesta == null) ? "ERR" : "OK";
+
 }
