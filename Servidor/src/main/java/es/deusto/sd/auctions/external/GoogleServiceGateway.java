ab118b2bb7bae6d113d20ff4829cdbf400392049
MarkelUrquiza <markel.urquiza@opendeusto.es>
2024-12-04 21:53:58 +0100

    cambios en el properties y en el googlegateway

--- Servidor/src/main/java/es/deusto/sd/auctions/external/GoogleServiceGateway.java
+++ Servidor/src/main/java/es/deusto/sd/auctions/external/GoogleServiceGateway.java
@@ -1,5 +1,23 @@
 package es.deusto.sd.auctions.external;
 
-public class GoogleServiceGateway {
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+@Component
+public class GoogleServiceGateway implements ServiceGateway {
+
+    private static final String GOOGLE_BASE_URL = "http://localhost:8080/google";
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Override
+    public boolean verifyEmail(String email) {
+        String url = GOOGLE_BASE_URL + "/verify/" + email;
+        return restTemplate.getForObject(url, Boolean.class);
+    }
+
+    @Override
+    public boolean validateLogin(String email, String password) {
+        String url = GOOGLE_BASE_URL + "/validate?email=" + email + "&password=" + password;
+        return restTemplate.postForObject(url, null, Boolean.class);
+    }
 }
