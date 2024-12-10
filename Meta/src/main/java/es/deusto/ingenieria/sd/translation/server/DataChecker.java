1a0927af9c1273869fd1d2e9dcac1f7745c7f01b
Endika Fernandez <fernandez.endika@opendeusto.es>
2024-12-10 17:45:03 +0100

    eliminacion de clase, actualizacion de datos de inicio y mapa de meta

--- Meta/src/main/java/es/deusto/ingenieria/sd/translation/server/DataChecker.java
+++ Meta/src/main/java/es/deusto/ingenieria/sd/translation/server/DataChecker.java
@@ -1,27 +1,17 @@
 package es.deusto.ingenieria.sd.translation.server;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
-
-import org.json.JSONArray;
 
 public class DataChecker {
 	
 	private Map<String, String> emails = new HashMap<>();
 	
 	public void inicializarEmails() {
-	    emails.put("usuario1@ejemplo.com", "password123");
-	    emails.put("usuario2@ejemplo.com", "1234abcd");
-	    emails.put("usuario3@ejemplo.com", "miContraseña");
+	    emails.put("paco@meta.com", "password123");
+	    emails.put("maria@meta.com", "1234abcd");
+	    emails.put("help@meta.com", "ABCD1234");
+	    emails.put("contact@meta.com", "1a2b3c4d");
 	}
 
 	
